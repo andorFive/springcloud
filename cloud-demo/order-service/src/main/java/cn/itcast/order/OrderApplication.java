95ac861d5716ab1ead56074d011bae38cda9c723
andor_5 <1743010385@qq.com>
2022-12-07 16:50:00 +0800

    ✨Feign

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients(clients = {UserClient.class})
 @MapperScan("cn.itcast.order.mapper")
