dc036868081c9bcf02944d60444e4324a18924b9
andor_5 <1743010385@qq.com>
2022-12-07 17:03:45 +0800

    ✨gateway

---
+++ cloud-demo/gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author andorFive
+ * @date 2022/12/7
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
